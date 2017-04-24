e6d7eacd0292423cee225149452c60fa249eb4fa
txm241 <tommy.may@sherwin.com>
2017-04-24 17:12:20 -0400

    Working bootstrap grid and google material components insalled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'itmazyiii-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'itmazyiii works!';
+    title = 'app works!';
 }
