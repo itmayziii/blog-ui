e4e56b8479e80dbf90e63a85d0fe984238f99b10
txm241 <tommy.may@sherwin.com>
2017-04-29 12:55:49 -0400

    Created a reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -23,7 +23,7 @@ import { HeaderComponent } from './header/header.component';
     ],
     imports: [
         BrowserModule,
-        FormsModule,
+        ReactiveFormsModule,
         HttpModule,
         AppRoutingModule,
         BrowserAnimationsModule,
--- src/app/contact/create-form/create-form.component.ts
+++ src/app/contact/create-form/create-form.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'tm3-contact-create-form',
     templateUrl: './create-form.component.html',
     styleUrls: ['./create-form.component.scss']
 })
-export class ContactCreateFormComponent implements OnInit {
+export class ContactCreateFormComponent {
+    private contactCreateForm: FormGroup;
 
-    constructor() { }
-
-    ngOnInit() {
+    constructor(private formBuilder: FormBuilder) {
+        this.createForm();
     }
 
+    private createForm() {
+        this.contactCreateForm = this.formBuilder.group({
+            "first-name": this.formBuilder.control(null),
+            "last-name": this.formBuilder.control(null),
+            "email": this.formBuilder.control(null),
+            "comments": this.formBuilder.control(null)
+        })
+    }
+
+    private onSubmit() {
+        console.log(this.contactCreateForm.value);
+    }
 }
