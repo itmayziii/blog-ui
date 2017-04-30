a2b03991a5c5d2cd01d436bc06f62f3e324f1dc8
itmayziii <rangertmay@gmail.com>
2017-04-30 15:24:11 -0400

    More merge conflic resolution

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { ContactCreateFormComponent } from './contact/create-form/contact-create-form.component';
-=======
-import { ContactCreateFormComponent } from './contact/create-form/create-form.component';
->>>>>>> master
+
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 const routes: Routes = [
