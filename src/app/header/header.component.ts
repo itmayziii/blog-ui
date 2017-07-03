23578573dea954aaba6d86bef29e8a05e0533dee
itmayziii <rangertmay@gmail.com>
2017-07-02 21:26:36 -0400

    Modified the header to use bootstraps navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavLink } from "../models/nav-link";
 
 @Component({
     selector: 'app-header',
     template: `
         <header>
-            <nav class="main-navigation">
-              <div>  
-                  <a href="/html/">Blog</a>
-                  <a href="/css/">Contact</a>
-              </div>
+            <nav class="navbar navbar-toggleable-md sticky-top navbar-light bg-primary">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#collapsible-nav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#">Navbar</a>
+                
+                <div class="collapse navbar-collapse" id="collapsible-nav">
+                    <ul class="navbar-nav">
+                        <li *ngFor="let link of links" class="nav-item"><a href="{{link.path}}" class="nav-link">{{link.title}}</a></li>
+                    </ul>
+                </div>
             </nav>
-            
+   
         </header>
 
   `,
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+    public links: Array<NavLink> = [
+        {title: 'Blog', path: '/blog'},
+        {title: 'Contact', path: '/contact'},
+        {title: 'Login', path: '/login'}
+    ];
 }
