a0958a68c07a59db49b4fc947bd6abdddfbc8aa0
itmayziii <rangertmay@gmail.com>
2017-07-04 19:41:14 -0400

    Making progress for angular like input field to work

--- src/app/fields/input/input.component.ts
+++ src/app/fields/input/input.component.ts
@@ -1,14 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input } from '@angular/core';
 
 @Component({
     selector: 'blog-input',
     template: `
-    <label for="{{fieldName}}" class="input-label">{{fieldLabel}}</label>
-    <ng-content select="input"></ng-content>
+    <fieldset class="form-group" (click)="active = true" (keyup.tab)="toggleActive()">
+        <label for="{{fieldName}}" class="input-label" [class.active]="active">{{fieldLabel}}</label>
+        <ng-content select="input"></ng-content>
+    </fieldset>
   `,
     styleUrls: ['./input.component.scss']
 })
 export class InputComponent {
     @Input() public fieldName: string = '';
     @Input() public fieldLabel: string = '';
+    public active: boolean = false;
+
+    public constructor(private eref: ElementRef) {
+
+    }
+
+    public toggleActive() {
+        this.active = !this.active;
+    }
+
+    @HostListener('document:click', ['$event'])
+    public onDocumentClick(event) {
+        let elementClicked = this.eref.nativeElement.contains(event.target);
+        if (!elementClicked && this.active === true) {
+            this.toggleActive();
+        }
+    }
 }
