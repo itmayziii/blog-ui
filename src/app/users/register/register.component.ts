12a214b0da13615c626ab3f251114869592a3b4a
itmayziii <rangertmay@gmail.com>
2017-07-25 17:06:41 -0400

    Made changes to register component, mostly stying

--- src/app/users/register/register.component.ts
+++ src/app/users/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { HttpService } from "../../services/http/http.service";
+import { Subscription } from "rxjs/Subscription";
+import { EqualValidator } from "../../validators/equal-validator.directive";
 
 @Component({
     selector: 'blog-register',
@@ -17,16 +19,17 @@ export class RegisterComponent implements OnInit {
     }
 
     public onRegister() {
-        
+
     }
 
     private createForm() {
+        let equalValidator = new EqualValidator();
         this.registerForm = this.formBuilder.group({
             "first-name": [null, [Validators.required]],
             "last-name": [null, [Validators.required]],
             "email": [null, [Validators.required]],
             "password": [null, [Validators.required]],
-            "password-confirmation": [null, [Validators.required]]
-        })
+            "password-confirmation": [null, [Validators.required, equalValidator.validate]]
+        });
     }
 }
