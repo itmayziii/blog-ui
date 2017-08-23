433834550fd751b921284793a290bfe5bf6ed0a5
itmayziii <rangertmay@gmail.com>
2017-08-23 16:18:35 -0400

    Modified navbar to handle toggling when a link is clicked

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavLink } from "../models/nav-link";
 
 @Component({
@@ -6,17 +6,17 @@ import { NavLink } from "../models/nav-link";
     template: `
         <header>
             <nav class="navbar navbar-toggleable-md sticky-top navbar-light bg-primary">
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#collapsible-nav">
+                <button class="navbar-toggler navbar-toggler-right" type="button" (click)="toggleNavigationMenu()">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <a class="navbar-brand" href="#">
                     Placeholder
                 </a>
 
-                <div class="collapse navbar-collapse" id="collapsible-nav">
+                <div class="collapse navbar-collapse" [class.show]="!navbarCollapsed">
                     <ul class="navbar-nav">
                         <li *ngFor="let link of links" class="nav-item">
-                            <a routerLink="{{link.path}}" routerLinkActive="active" class="nav-link">{{link.title}}</a>
+                            <a routerLink="{{link.path}}" (click)="toggleNavigationMenu()" routerLinkActive="active" class="nav-link">{{link.title}}</a>
                         </li>
                     </ul>
                 </div>
@@ -26,9 +26,15 @@ import { NavLink } from "../models/nav-link";
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+    @ViewChild('collapsibleNav') public collapsibleNav: ElementRef;
+    public navbarCollapsed: boolean = true;
     public links: Array<NavLink> = [
         {title: 'Blog', path: '/blog'},
         {title: 'Contact', path: '/contacts/create'},
         {title: 'Login', path: '/users/login'}
     ];
+
+    public toggleNavigationMenu() {
+        this.navbarCollapsed = !this.navbarCollapsed;
+    }
 }
