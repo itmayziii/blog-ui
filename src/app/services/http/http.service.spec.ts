ea534949a635fabb6fe705515b86a2556de41a47
itmayziii <rangertmay@gmail.com>
2017-08-22 21:56:49 -0400

    Added tests to http service for post() method

--- src/app/services/http/http.service.spec.ts
+++ src/app/services/http/http.service.spec.ts
@@ -48,12 +48,12 @@ describe('http.service.ts', () => {
             })();
         });
 
-        it('if an error occurs with the request then the router navigates to the login page', (done) => {
+        it('should navigate to the login page if an authentication error occurs', (done) => {
             inject([HttpService, Router], (httpService: HttpService, router: Router) => {
                 makeBadHttpRequest();
                 spyOn(router, 'navigate');
 
-                httpService.get('users').subscribe((response) => {
+                httpService.get('users').subscribe(() => {
                     expect(router.navigate).toHaveBeenCalledTimes(1);
                     expect(router.navigate).toHaveBeenCalledWith(['/users/login']);
                     done();
@@ -78,7 +78,46 @@ describe('http.service.ts', () => {
 
     describe('post()', () => {
 
-        it('should make an HTTP POST request')
+        it('should make an HTTP POST request to the correct url with the correct options', (done) => {
+            inject([HttpService], (httpService: HttpService) => {
+                makeSuccessfulHttpRequest((connection: MockConnection) => {
+                    expect(connection.request.url.endsWith('users')).toBeTruthy();
+                    expect(connection.request.headers.toJSON()).toEqual({"Content-Type": ['JSON']});
+                });
+
+                httpService.post('users', {}).subscribe((response) => {
+                    expect(response.data).toBeDefined();
+                    expect(response.data).toBe("Awesome Test");
+                    done();
+                });
+            })();
+        });
+
+        it('should navigate to the login page if an authentication error occurs', (done) => {
+            inject([HttpService, Router], (httpService: HttpService, router: Router) => {
+                makeBadHttpRequest();
+                spyOn(router, 'navigate');
+
+                httpService.post('users', {}).subscribe(() => {
+                    expect(router.navigate).toHaveBeenCalledTimes(1);
+                    expect(router.navigate).toHaveBeenCalledWith(['/users/login']);
+                    done();
+                });
+            })();
+        });
+
+        it('should allow passed in headers to override default headers', (done) => {
+            inject([HttpService], (httpService: HttpService) => {
+                makeSuccessfulHttpRequest((connection: MockConnection) => {
+                    expect(connection.request.headers.toJSON()).toEqual({"Content-Type": ["text/plain"]});
+                });
+
+                const headers = new Headers({"Content-Type": "text/plain"});
+                httpService.post('users', {}, {headers: headers}).subscribe(() => {
+                    done();
+                });
+            })();
+        });
 
     });
 });
