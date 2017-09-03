0ab7e82f9acf83ef63006aad6a271b05677ad902
itmayziii <rangertmay@gmail.com>
2017-09-03 12:55:40 -0400

    Header now has right and left side

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -15,8 +15,26 @@ import { NavLink } from "../models/nav-link";
 
                 <div class="collapse navbar-collapse" [class.show]="!navbarCollapsed">
                     <ul class="navbar-nav">
-                        <li *ngFor="let link of links" class="nav-item">
-                            <a routerLink="{{link.path}}" (click)="toggleNavigationMenu()" routerLinkActive="active" class="nav-link">{{link.title}}</a>
+                        <li *ngFor="let link of leftLinks" class="nav-item">
+                            <a routerLink="{{link.path}}"
+                               (click)="toggleNavigationMenu()"
+                               routerLinkActive="active"
+                               class="nav-link"
+                               *ngIf="(link.condition) ? link.condition() : true"
+                            >{{link.title}}</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="collapse navbar-collapse" [class.show]="!navbarCollapsed">
+                    <ul class="navbar-nav ml-auto">
+                        <li *ngFor="let link of rightLinks" class="nav-item">
+                            <a routerLink="{{link.path}}"
+                               (click)="toggleNavigationMenu()"
+                               routerLinkActive="active"
+                               class="nav-link"
+                               *ngIf="(link.condition) ? link.condition() : true"
+                            >{{link.title}}</a>
                         </li>
                     </ul>
                 </div>
@@ -28,14 +46,26 @@ import { NavLink } from "../models/nav-link";
 export class HeaderComponent {
     @ViewChild('collapsibleNav') public collapsibleNav: ElementRef;
     public navbarCollapsed: boolean = true;
-    public links: Array<NavLink> = [
-        {title: 'Blog', path: '/blog'},
-        {title: 'Contact', path: '/contacts/create'},
-        {title: 'Login', path: '/users/login'},
-        {title: 'Register', path: '/users/register'}
+    public leftLinks: Array<NavLink> = [
+        {title: 'Blog', path: '/blog',},
+        {title: 'Contact', path: '/contacts/create'}
     ];
+    public rightLinks: Array<NavLink> = [
+        {title: 'Login', path: '/users/login', condition: () => !this.isUserLoggedIn()},
+        {title: 'Register', path: '/users/register', condition: () => !this.isUserLoggedIn()},
+        {
+            title: 'Logout',
+            path: '/users/logout',
+            condition: this.isUserLoggedIn
+        }
+    ];
+
 
     public toggleNavigationMenu() {
         this.navbarCollapsed = !this.navbarCollapsed;
+    }
+
+    public isUserLoggedIn(): boolean {
+        return (localStorage.getItem('API-Token')) ? true : false;
     }
 }
