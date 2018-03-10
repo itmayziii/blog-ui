d4eea00d1f5ef2d0756119f9e72f3ef5c43148d8
itmayziii <tommymay37@gmail.com>
2018-03-10 17:49:57 -0500

    Added a loader for the pages.

---
+++ src/app/loader/loader.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'blog-loader',
+    styleUrls: ['./loader.component.scss'],
+    template: `
+        <div class="loader-container">
+            <div class="loader"></div>
+        </div>
+    `
+})
+export class LoaderComponent {
+
+}
