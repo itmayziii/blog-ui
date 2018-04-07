a9b03e653743036231f8e433a86befae21eb30ce
Tommy May <tommymay37@gmail.com>
2018-04-07 11:56:26 -0400

    Feature/ukqedgsc pages support (#23)
    * Updated header to have a better dropdown experience
    
    * Now have navbar updating when a link is clicked
    
    * Removing border when app is loading
    
    * removed comments

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,54 +1,56 @@
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Inject, OnDestroy, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import { NavLink } from '../models/nav-link';
 import { UserService } from '../services/user.service';
 import { LoadingService } from '../services/loading.service';
 import { ISubscription } from 'rxjs/Subscription';
+import { isPlatformBrowser } from '@angular/common';
+
+declare var $: any;
 
 @Component({
     selector: 'blog-header',
     template: `
-        <header class="header sticky-top">
-            <nav class="navbar navbar-expand-md navbar-light py-0">
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top">
 
-                <span *ngIf="isAppLoading" class="navbar-brand navbar-brand--loading"><blog-loader size="1.2rem"></blog-loader></span>
-                <a *ngIf="!isAppLoading" class="navbar-brand text-success" routerLink="/">FHD</a>
+            <span *ngIf="isAppLoading" class="navbar-brand navbar-brand--loading"><blog-loader size="1.2rem"></blog-loader></span>
+            <a *ngIf="!isAppLoading" class="navbar-brand text-success" routerLink="/">FHD</a>
 
-                <button class="navbar-toggler navbar-toggler-right" type="button" (click)="toggleNavigationMenu()">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <button (click)="swapTogglerIcon()" class="navbar-toggler ml-auto" type="button" data-toggle="collapse"
+                    data-target="#navbarItems"
+                    aria-controls="navbarItems"
+                    aria-label="Toggle navigation"><i #navbarToggler class="fa fa-bars navbar-toggler-hamburger" aria-hidden="true"></i>
+            </button>
 
-                <div class="collapse navbar-collapse" [class.show]="!isNavigationCollapsed">
-                    <ul class="navbar-nav">
-                        <li *ngFor="let rightLink of leftLinks" class="nav-item">
-                            <a routerLink="{{rightLink.path}}"
-                               (click)="toggleNavigationMenu()"
-                               routerLinkActive="active"
-                               class="nav-link"
-                               *ngIf="rightLink.condition()"
-                            >{{rightLink.title}}</a>
-                        </li>
-                    </ul>
-                </div>
+            <div class="collapse navbar-collapse mt-2 mt-md-0 text-center" id="navbarItems">
+                <ul class="navbar-nav mr-auto">
+                    <li *ngFor="let leftLink of leftLinks" class="nav-item">
+                        <a routerLink="{{ leftLink.path }}"
+                           (click)="closeNavbar()"
+                           routerLinkActive="active"
+                           class="nav-link"
+                           *ngIf="leftLink.condition()"
+                        >{{leftLink.title}}</a>
+                    </li>
+                </ul>
 
-                <div class="collapse navbar-collapse" [class.show]="!isNavigationCollapsed">
-                    <ul class="navbar-nav ml-auto">
-                        <li *ngFor="let leftLink of rightLinks" class="nav-item">
-                            <a routerLink="{{leftLink.path}}"
-                               (click)="toggleNavigationMenu()"
-                               routerLinkActive="active"
-                               class="nav-link"
-                               *ngIf="leftLink.condition()"
-                            >{{leftLink.title}}</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+                <ul class="navbar-nav ml-auto">
+                    <li *ngFor="let rightLink of rightLinks" class="nav-item">
+                        <a routerLink="{{ rightLink.path }}"
+                           (click)="closeNavbar()"
+                           routerLinkActive="active"
+                           class="nav-link"
+                           *ngIf="rightLink.condition()"
+                        >{{ rightLink.title }}</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     `,
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-    @ViewChild('collapsibleNav') public collapsibleNav: ElementRef;
+    @ViewChild('navbarToggler') public navbarToggler: ElementRef;
+
     public isNavigationCollapsed: boolean = true;
     public isAppLoading: boolean = false;
     private isAppLoadingSubscription: ISubscription;
@@ -80,7 +82,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
         {title: 'Logout', path: '/users/logout', condition: () => this.userService.isLoggedIn()}
     ];
 
-    public constructor(private userService: UserService, private loadingService: LoadingService) {
+    public constructor(private userService: UserService, private loadingService: LoadingService, @Inject(PLATFORM_ID) private platformId: Object) {
     }
 
     public ngOnInit(): void {
@@ -93,8 +95,25 @@ export class HeaderComponent implements OnInit, OnDestroy {
         }
     }
 
-    public toggleNavigationMenu() {
-        this.isNavigationCollapsed = !this.isNavigationCollapsed;
+    public swapTogglerIcon() {
+        const isClosed = this.navbarToggler.nativeElement.classList.contains('fa-bars');
+        if (isClosed) {
+            this.navbarToggler.nativeElement.classList.remove('fa-bars');
+            this.navbarToggler.nativeElement.classList.add('fa-times');
+            return;
+        }
+
+        this.navbarToggler.nativeElement.classList.remove('fa-times');
+        this.navbarToggler.nativeElement.classList.add('fa-bars');
+    }
+
+    public closeNavbar() {
+        if (!isPlatformBrowser(this.platformId)) {
+            return;
+        }
+        this.swapTogglerIcon();
+
+        $('.navbar-collapse').collapse('hide');
     }
 
     private listenToAppLoading() {
